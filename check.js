195c7fb6451e7df10f86733ca219094e437b9e50
Mohannad Najjar <mohannadnaj@me.com>
2024-06-22 10:52:04 +0300

    init

--- check.js
+++ check.js
@@ -8,10 +8,15 @@ const REPO = 'google-search-count-userscript';
 
 (async () => {
   const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--lang=en-US']
   });
   const page = await browser.newPage();
-  await page.goto('https://www.google.com/search?q=puppeteer');
+
+  await page.setExtraHTTPHeaders({
+    'Accept-Language': 'en-US'
+  });
+
+  await page.goto('https://www.google.com/search?hl=en&q=puppeteer');
 
   const exists = await page.evaluate(() => {
     return !!document.getElementById('result-stats') && document.getElementById('appbar');
@@ -27,18 +32,37 @@ const REPO = 'google-search-count-userscript';
       state: 'open'
     });
 
-    const existingIssue = issues.data.find(issue => issue.title === 'Element not found error in check-google.js');
+    const existingIssue = issues.data.find(issue => issue.title === 'Google: Element not found error');
 
     if (!existingIssue) {
       await octokit.issues.create({
         owner: OWNER,
         repo: REPO,
-        title: 'Element not found error in check-google.js',
+        title: 'Google: Element not found error',
         body: 'The expected element was not found on the page. The cron job will be paused until this issue is resolved.'
       });
     }
 
     throw new Error('Element not found');
+  }
+
+  if (await page.evaluate(() => {
+    const elRect = document.getElementById('result-stats').getBoundingClientRect();
+    return elRect.top > 0;
+  })) {
+    throw new Error('Element is visible before appending');
+  }
+
+  await page.addScriptTag({ path: 'userscript.js' });
+
+  await page.waitForFunction(() => {
+    return window.dispatchEvent(new CustomEvent('load'));
+  });
+
+  if (! (await page.evaluate(() => {
+    return Array.from(document.querySelectorAll('#result-stats')).some(el => el.getBoundingClientRect().top > 0);
+  }))) {
+    throw new Error('Element is not visible after appending');
   }
 
   console.log('All good');
